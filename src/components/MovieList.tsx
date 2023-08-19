6ad8a0a492596301f9bbe7f14a77ed18f4f80f53
Steffen Andre Hagen <steffen.hagen@bidbax.no>
2023-08-19 17:03:28 +0200

    Fix some mobile issues

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -11,8 +11,8 @@ const MAX_ELEMENTS_TO_SHOW = 5;
 const MovieList = ({title, movies}:Props) => {
   return (
     <>
-    <h2 className={"text-2xl mt-20 mb-4 font-bold "}>{title}</h2>
-    <div className={"flex"}>
+    <h2 className={"text-2xl mt-20 mb-4 font-bold px-10 md:px-0"}>{title}</h2>
+    <div className={"flex flex-col md:flex-row px-10 md:px-0"}>
       {movies.slice(0, MAX_ELEMENTS_TO_SHOW).map((movie) => <Movie key={movie.id} movie={movie}/>)}
     </div>
     </>
